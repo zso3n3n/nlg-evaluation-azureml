cd6b4143c0278a0ff1e0c4fd6c35a08748aaf8af
zsGBB <zacksoenen@microsoft.com>
2024-01-26 12:46:46 -0600

    added cosine similarity to rag eval flow.

--- src/promptflow/evaluation_flows/rag_eval/concat_scores.py
+++ src/promptflow/evaluation_flows/rag_eval/concat_scores.py
@@ -4,13 +4,15 @@ from promptflow import tool
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
 @tool
-def concat_scores(question: str, answer: str, context: str, groundedness_answer: str, goundedness_question: str) -> dict:
+def concat_scores(question: str, answer: str, context: str, groundedness_answer: str, goundedness_question: str, question_context_sim: float, answer_context_sim: float) -> dict:
 
     output_json = {'question': question,
                    'answer': answer,
                    'context': context,
                    'groundedness_answer': groundedness_answer,
-                   'goundedness_question': goundedness_question
+                   'goundedness_question': goundedness_question,
+                   'question_context_sim' : question_context_sim,
+                   'answer_context_sim' : answer_context_sim
                    }
     
     return output_json
